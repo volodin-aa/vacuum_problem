b08d15a008b6f69cfa0de4ef99e128e8184a7eb5
Volodin AA <volodin_aa@taximaxim.ru>
2015-03-10 09:54:53 +0400

    PostgreSQL 9.3 (1M records), обновление записей после запуска параллельной транзакци

--- README.md
+++ README.md
@@ -41,3 +41,35 @@ VACUUM
 11. Run VACUUM FULL
 VACUUM
 12. After VACUUM FULL table size is: 58736640
+
+
+PostgreSQL 9.3 (1M records), обновление записей после запуска параллельной транзакции
+-------------------
+
+1. Create table vacuum_table
+CREATE TABLE
+2. Insert 1000000 records into vacuum_table
+INSERT 0 1000000
+3. After insert 1000000 records table size is: 58769408
+4. Check autovacuum: AUTOVACUUM is on
+5. Update table vacuum_table
+UPDATE 1000000
+6. After update 1000000 records table size is: 140017664
+7. Run long live transaction pid: 32474
+8. Run VACUUM FULL
+VACUUM
+9. After VACUUM FULL table size is: 58736640
+10. New update table vacuum_table
+UPDATE 1000000
+11. Run VACUUM FULL
+VACUUM
+12. After new update and VACUUM FULL table size is: 117448704
+13. Second update table vacuum_table
+UPDATE 1000000
+14. Run VACUUM FULL
+VACUUM
+15. After second update and VACUUM FULL table size is: 176152576
+16. Killing long live transaction
+17. Run VACUUM FULL
+VACUUM
+18. After killing and VACUUM FULL table size is: 176152576
---
+++ problem_unsolved.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+
+RECORDS=1000000
+
+PSQL="psql -p 5433 -X"
+
+
+
+echo 1. Create table vacuum_table
+
+$PSQL -1 -c "drop table if exists vacuum_table; create table vacuum_table (id integer primary key)"
+
+echo 2. Insert $RECORDS records into vacuum_table
+
+$PSQL -1 -c "insert into vacuum_table (id) (select generate_series(1, $RECORDS) id)"
+
+TSIZE=$($PSQL -At -v table_schema="'public'" -v table_name="'vacuum_table'" -f table_size.sql)
+
+echo 3. After insert $RECORDS records table size is: $TSIZE
+
+echo 4. Check autovacuum: AUTOVACUUM is $($PSQL -At -c 'show autovacuum')
+
+echo 5. Update table vacuum_table
+
+$PSQL -1 -c "update vacuum_table set id = id"
+
+TSIZE=$($PSQL -X -At -v table_schema="'public'" -v table_name="'vacuum_table'" -f table_size.sql)
+
+echo 6. After update $RECORDS records table size is: $TSIZE
+
+$PSQL -1 -c 'select pg_sleep(120)' &
+PID=$!
+
+echo 7. Run long live transaction pid: $PID
+
+echo 8. Run VACUUM FULL
+
+$PSQL -1 -c 'VACUUM FULL'
+
+TSIZE=$($PSQL -X -At -v table_schema="'public'" -v table_name="'vacuum_table'" -f table_size.sql)
+
+echo 9. After VACUUM FULL table size is: $TSIZE
+
+echo 10. New update table vacuum_table
+
+$PSQL -1 -c "update vacuum_table set id = id"
+
+TSIZE=$($PSQL -X -At -v table_schema="'public'" -v table_name="'vacuum_table'" -f table_size.sql)
+
+echo 11. Run VACUUM FULL
+
+$PSQL -1 -c 'VACUUM FULL'
+
+TSIZE=$($PSQL -X -At -v table_schema="'public'" -v table_name="'vacuum_table'" -f table_size.sql)
+
+
+echo 12. After new update and VACUUM FULL table size is: $TSIZE
+
+echo 13. Second update table vacuum_table
+
+$PSQL -1 -c "update vacuum_table set id = id"
+
+TSIZE=$($PSQL -X -At -v table_schema="'public'" -v table_name="'vacuum_table'" -f table_size.sql)
+
+echo 14. Run VACUUM FULL
+
+$PSQL -1 -c 'VACUUM FULL'
+
+TSIZE=$($PSQL -X -At -v table_schema="'public'" -v table_name="'vacuum_table'" -f table_size.sql)
+
+
+echo 15. After second update and VACUUM FULL table size is: $TSIZE
+
+echo 16. Killing long live transaction
+
+kill -9 $PID
+
+echo 17. Run VACUUM FULL
+
+$PSQL -1 -c 'VACUUM FULL'
+
+TSIZE=$($PSQL -X -At -v table_schema="'public'" -v table_name="'vacuum_table'" -f table_size.sql)
+
+echo 18. After killing and VACUUM FULL table size is: $TSIZE
+
